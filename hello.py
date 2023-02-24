1c17a03823e23e5eb7ded2ad578b7fdb428b2c97
Jeff Soh <jeffswr9320@gmail.com>
2023-02-24 18:36:07 +0800

    Added Name.html, imported flask_wtform

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "123456"
+
+
+# Create form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator
-
-
 @app.route('/')
 def index():
     # FILTERS
@@ -26,23 +36,35 @@ def index():
 
 
 # localhost:5000/user/john
-
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
 
 # Create Custom Error Pages
 
+
 # Invalid URL
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
+
 # Internal Server Error Thing
-
-
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Va;idate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                           name=name,
+                           form=form
+                           )
